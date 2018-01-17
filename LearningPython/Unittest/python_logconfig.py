5b7a4fb1d56143ab1639ca17acca106dfae1d597
dingxd287 <dingxd@13322.com>
2018-01-17 19:57:06 +0800

    qqq

---
+++ LearningPython/Unittest/python_logconfig.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(filename)s [line:%(lineno)d] %(levelname)s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+
+)
