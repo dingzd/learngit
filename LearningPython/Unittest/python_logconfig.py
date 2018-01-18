ac9cb3e132cabcbe24df470f2beb3f0b5a10d246
dingxd287 <dingxd@13322.com>
2018-01-18 12:02:38 +0800

    logconfig

--- LearningPython/Unittest/python_logconfig.py
+++ LearningPython/Unittest/python_logconfig.py
@@ -5,5 +5,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s %(filename)s [line:%(lineno)d] %(levelname)s %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
+    filename='report1.log',
+    filemode='w'
 
 )
