cce4170c3bd5065edf48d759992c5fec9ad105e2
dingxd287 <dingxd@13322.com>
2018-01-25 17:21:07 +0800

    add login model

--- LearningPython/Unittest/login.py
+++ LearningPython/Unittest/login.py
@@ -7,20 +7,28 @@ import ddt
 @ddt.ddt
 class Login(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome()
+        self.option = webdriver.ChromeOptions()
+        self.option.add_argument('disable-infobars')
+        self.driver = webdriver.Chrome(chrome_options=self.option)
 
     @ddt.data(("18600000002","123456"))
     @ddt.unpack
     def test_login(self,phone,password):
         url = "https://www.htxksport.com"
         self.driver.get(url)
-        time.sleep(3)
         self.driver.maximize_window()
-
         self.driver.implicitly_wait(5)
-
-        self.driver.find_element_by_link_text("登录").click()
-        self.driver.find_element_by_id("el-input-group--prepend").send_keys(phone)
+        try:
+            self.driver.find_element_by_link_text("登录").click()
+            self.driver.find_element_by_xpath("//*[@id='__nuxt']/div[2]/div[2]/div[2]/div/form/div[3]/div/div[1]/input").send_keys(
+            phone)
+            self.driver.find_element_by_xpath("//*[@id='__nuxt']/div[2]/div[2]/div[2]/div/form/div[4]/div/div/div/input").send_keys(
+            password)
+            self.driver.find_element_by_xpath("//*[@id='__nuxt']/div[2]/div[2]/div[2]/div/form/div[5]/div/button").click()
+            # self.assertTrue(expactdata in self.driver.page_source)
+            self.assertTrue(self.driver.find_element_by_link_text("资讯"))
+        except Exception as e:
+            print(e)
 
 
     def tearDown(self):
